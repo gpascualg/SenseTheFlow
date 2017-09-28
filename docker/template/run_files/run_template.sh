a900a82ad73c70670b2d09c3c108042db0d293d9
gpascualg <gpascualg93@gmail.com>
2017-09-28 18:08:46 +0200

    Minor improvements to run_template

--- docker/template/run_files/run_template.sh
+++ docker/template/run_files/run_template.sh
@@ -2,8 +2,12 @@ RAW_PASS="<password>"
 SHA_PASS=$(python -c "from notebook.auth import passwd; print(passwd('$RAW_PASS'))")
 
 if [[ $? -ne 0 ]]; then
-    echo 'Fatal error, could not find anaconda python installed'
-    exit 1
+    SHA_PASS=$(docker run -i continuumio/anaconda3 python -c "from notebook.auth import passwd; print(passwd('$RAW_PASS'))")
+
+    if [[ $? -ne 0 ]]; then
+        echo 'Fatal error, could not find anaconda python installed'
+        exit 1
+    fi
 fi
 
 ../run_docker.sh --name <username> -s ../ssh_keys/<username>.pub -p 8888:8888 -p 6006:6006 -p 2200:22 -v /data:/data -v /notebooks:/notebooks -e JUPYTER_PASSWORD='$SHA_PASS' -e FETCH_TF_CONTRIB=1 deepstack
