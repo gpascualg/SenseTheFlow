d7919ef925f13ea67218e0ed668e7808bab1cf6a
gpascualg <gpascualg93@gmail.com>
2018-03-19 13:34:54 +0100

    RocksStore concating

--- rocksdb.py
+++ rocksdb.py
@@ -9,12 +9,46 @@ import tqdm as tq # conda install -c conda-forge tqdm
 import traceback
 
 
+class RocksConcat(object):
+    def __init__(self, stores, elements_per_iter):
+        self.stores = stores
+        self.elements_per_iter = elements_per_iter
+
+    def iterate(self, cyclic=True):
+        has_done_epoch = [False] * len(self.stores)
+        itrs = [store.iterate(cyclic=cyclic) for store in self.stores]
+
+        while True:
+            if all(has_done_epoch) and not cyclic:
+                raise StopIteration()
+
+            for pos in range(len(self.stores)):
+                results = []
+                itr = itrs[pos]
+
+                while len(results) < self.elements_per_iter[pos]:
+                    try:
+                        results.append(next(itr))
+                    except StopIteration:
+                        itr = itrs[pos] = self.stores[pos].iterate(cyclic=cyclic)
+                        has_done_epoch[pos] = True
+
+                yield tuple(results)
+
+    def concat(self, other, elements_per_iter):
+        self.stores.append(other)
+        self.elements_per_iter.append(elements_per_iter)
+
+
 class RocksStore(object):
     def __init__(self):
         self.dbs = []
 
     def add(self, db, shape=None):
         self.dbs.append((db, shape))
+
+    def concat(self, other, elements_per_iter_self=1, elements_per_iter_other=1):
+        return RocksConcat([self, other], [elements_per_iter_self, elements_per_iter_other])
 
     def iterate(self, cyclic=True):
         itrs = [db.iterate(shape, cyclic=cyclic) for db, shape in self.dbs]
