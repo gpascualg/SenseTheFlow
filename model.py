e22d5db0cba32976ddc8b8d4dc26d9efd5c79244
gpascualg <gpascualg93@gmail.com>
2018-07-17 11:34:37 +0200

    Add model freezing

--- model.py
+++ model.py
@@ -429,6 +429,54 @@ class Model(object):
         return generatorSetup.generator
 
 
+    # @https://blog.metaflow.fr/tensorflow-how-to-freeze-a-model-and-serve-it-with-a-python-api-d4f3596b3adc
+    def freeze(self, output_node_names, frozen_model_name='frozen_model.pb'):
+        model_dir = self.classifier().model_dir
+        if not tf.gfile.Exists(model_dir):
+            raise AssertionError(
+                "Export directory doesn't exists. Please specify an export "
+                "directory: %s" % model_dir)
+
+        # We retrieve our checkpoint fullpath
+        checkpoint = tf.train.get_checkpoint_state(model_dir)
+        input_checkpoint = checkpoint.model_checkpoint_path
+        
+        # We precise the file fullname of our freezed graph
+        absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
+        output_graph = absolute_model_dir + "/" + frozen_model_name
+
+        # We clear devices to allow TensorFlow to control on which device it will load operations
+        clear_devices = True
+
+        # We start a session using a temporary fresh Graph
+        with tf.Session(graph=tf.Graph()) as sess:
+            # We import the meta graph in the current default Graph
+            saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=clear_devices)
+
+            # We restore the weights
+            saver.restore(sess, input_checkpoint)
+
+            if not output_node_names:
+                print("You need to supply a list with one or more names of nodes to output_node_names. Either of")
+                for op in tf.get_default_graph().get_operations(): 
+                    print (op.name())
+                return False
+
+            # We use a built-in TF helper to export variables to constants
+            output_graph_def = tf.graph_util.convert_variables_to_constants(
+                sess, # The session is used to retrieve the weights
+                tf.get_default_graph().as_graph_def(), # The graph_def is used to retrieve the nodes 
+                output_node_names.split(",") # The output node names are used to select the usefull nodes
+            ) 
+
+            # Finally we serialize and dump the output graph to the filesystem
+            with tf.gfile.GFile(output_graph, "wb") as f:
+                f.write(output_graph_def.SerializeToString())
+            print("%d ops in the final graph." % len(output_graph_def.node))
+
+        return output_graph_def
+
+
 class ExecutionWrapper(object):
     def __init__(self, model, thread):
         self.model = model
