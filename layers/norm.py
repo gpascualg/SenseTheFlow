713bcab2efb1a1c1a18952dbfa6ed05e14e85ea0
gpascualg <gpascualg93@gmail.com>
2018-08-27 09:31:23 +0200

    Improvemnts to norm parameters

--- layers/norm.py
+++ layers/norm.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from .utils import channel_dim, to_data_format
 
 
 class GroupNorm(tf.layers.Layer):
@@ -13,13 +14,13 @@ class GroupNorm(tf.layers.Layer):
     # Create a trainable weight variable for this layer.
     self.gamma = self.add_variable(
         name='gamma', 
-        shape=(input_shape[1 if self.data_format == 'channels_first' else 3],),
+        shape=(input_shape[channel_dim(self.data_format)],),
         initializer=tf.constant_initializer(1.0),
         trainable=True
     )
     self.beta = self.add_variable(
         name='beta', 
-        shape=(input_shape[1 if self.data_format == 'channels_first' else 3],),
+        shape=(input_shape[channel_dim(self.data_format)],),
         initializer=tf.constant_initializer(0.0),
         trainable=True
     )
@@ -27,9 +28,7 @@ class GroupNorm(tf.layers.Layer):
     super(GroupNorm, self).build(input_shape)  # Be sure to call this somewhere!
 
   def call(self, x):
-    if self.data_format != 'channels_first':
-      x = tf.transpose(x, [0, 3, 1, 2])
-      
+    x = to_data_format(x, self.data_format, 'channels_first')
     N, C, H, W = x.shape.as_list()
     if N is None:
       N = tf.shape(x)[0]
@@ -43,24 +42,20 @@ class GroupNorm(tf.layers.Layer):
     beta = tf.reshape(self.beta, [1, C, 1, 1])
 
     output = tf.reshape(x, [N, C, H, W]) * gamma + beta
-        
-    # tranpose: [bs, c, h, w] to [bs, h, w, c] following the paper
-    if self.data_format != 'channels_first':
-      output = tf.transpose(output, [0, 2, 3, 1])
-    
+    output = to_data_format(x, 'channels_first', self.data_format)
     return output
 
   def compute_output_shape(self, input_shape):
     return input_shape
 
-def norm(x, norm_type, is_training, data_format, G=32, esp=1e-5):
+def norm(x, norm_type, is_training, data_format, G=32, esp=1e-5, momentum=0.999, epsilon=1e-5):
   if norm_type == 'none':
     output = x
   elif norm_type == 'batch':
     output = tf.layers.batch_norm(
-      x, center=True, scale=True, decay=0.999,
-      is_training=is_training, updates_collections=None
-    )
+      x, center=True, scale=True, momentum=momentum,
+      epsilon=epsilon, fused=True, axis=channel_dim(data_format),
+      is_training=is_training)
   elif norm_type == 'group':
     output = GroupNorm(G, esp, data_format)(x)
   else:
