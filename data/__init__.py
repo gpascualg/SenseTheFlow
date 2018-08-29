4b995cfef10d15f96e8eb1675093cf754a304f9f
gpascualg <gpascualg93@gmail.com>
2018-08-29 11:56:04 +0200

    Missing imports from previous commit

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,4 @@
 from .data_parser import DataParser
 
 from .rocksdb import RocksStore, RocksConcat
-from .rocksdb import RocksNumpy, RocksBytes, RocksString
+from .rocksdb import RocksNumpy, RocksNonConstantNumpy, RocksBytes, RocksString
--- data/rocksdb/__init__.py
+++ data/rocksdb/__init__.py
@@ -1,6 +1,6 @@
 from .groups.store import RocksStore
 from .groups.concat import RocksConcat
 
-from .loaders.numpy import RocksNumpy
+from .loaders.numpy import RocksNumpy, RocksNonConstantNumpy
 from .loaders.bytes import RocksBytes
 from .loaders.string import RocksString
