285e28c4811e1b7d7323625f1bab72401b02a0d9
gpascualg <gpascualg93@gmail.com>
2020-11-11 00:35:36 +0100

    Better default_config

--- SenseTheFlow/experiment/experiment.py
+++ SenseTheFlow/experiment/experiment.py
@@ -16,21 +16,20 @@ from .utils import discover
 from .mode import Mode, Hookpoint
 
                     
-def default_config(soft_device_placement=True):
+def default_config(soft_device_placement=True, log_device_placement=False):
+    tf.debugging.set_log_device_placement(log_device_placement)
     tf.config.set_soft_device_placement(soft_device_placement)
 
     # Currently, memory growth needs to be the same across GPUs
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
-        try:
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
 
-            logical_gpus = tf.config.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    else:
+        raise NotImplementedError("No GPUs found")
 
 class Experiment(object):
     Instances = {}
