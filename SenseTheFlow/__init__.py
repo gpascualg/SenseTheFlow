3a3e6ebb35e5d1bafb0ab2a71a325bc51381c4a7
gpascualg <gpascualg93@gmail.com>
2021-06-08 00:39:28 +0200

    Expose data sublib

--- SenseTheFlow/__init__.py
+++ SenseTheFlow/__init__.py
@@ -1,6 +1,6 @@
 from .data import DataParser
-from .data import summaries_to_dataframe
 from .data import RocksStore, RocksConcat
 from .data import RocksNumpy, RocksBytes, RocksString
 from . import contrib
+from . import data
 from . import experiment
